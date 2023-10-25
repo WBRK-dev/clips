6300ce123b4bb88cf41dab4aff03dd565b75863a
WBR_K-dev <koertjrweberp@gmail.com>
2023-10-25 10:33:42 +0200

    fixed merge conflict

--- root/index.js
+++ root/index.js
@@ -27,9 +27,7 @@ $(function() {
             $("video").attr("src", `clips/${selectedFolder}/${selectedClip}.mp4`);
             $("hero button").attr("shareurl-videolink", `https://wbrk-dev.github.io/clips/clips/${selectedFolder}/${selectedClip}.mp4`);
         });
-<<<<<<< HEAD
-=======
-
+        
         fetch(`clips/channels.json`)
         .then(j => j.json())
         .then(j => {
@@ -40,6 +38,5 @@ $(function() {
                 $("main .grid").append(`<a class="item" href="clipset/?set=${key}"><img src="clips/${key}/thumbnail.png"><p>${r[key].label}</p><p class="author">${channelLabel}</p></a>`);
             });
         });
->>>>>>> preview
     });
 });
