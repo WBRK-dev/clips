05b2db438d6a657522048411a3182f336c0f6f07
WBR_K-dev <koertjrweberp@gmail.com>
2023-10-25 09:46:27 +0200

    created a video randomizer

--- root/index.js
+++ root/index.js
@@ -8,4 +8,23 @@ $("hero #audiocontrol").click(function() {
     }
 });
 
-document.querySelector("video").addEventListener("contextmenu", function(e) {e.preventDefault()})
+document.querySelector("video").addEventListener("contextmenu", function(e) {e.preventDefault()});
+
+$(function() {
+    fetch("clips/metadata.json")
+    .then(r => r.json())
+    .then(r => {
+        let keys = Object.keys(r);
+        console.log(keys);
+        let selectedFolder = keys[Math.round((keys.length - 1) * Math.random())];
+
+        fetch(`clips/${selectedFolder}/metadata.json`)
+        .then(j => j.json())
+        .then(j => {
+            let clipKeys = Object.keys(j);
+            let selectedClip = clipKeys[Math.round((clipKeys.length - 1) * Math.random())];
+
+            $("video").attr("src", `clips/${selectedFolder}/${selectedClip}.mp4`);
+        });
+    });
+});
